44ac244652e549998ad480654d54b74fd8629c88
mashtoe <mashtoe@hotmail.com>
2018-04-19 18:27:35 +0200

    first commit

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform M_Player;
+    // Use this for initialization
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    {
+
+        GetComponent<NavMeshAgent>().destination = M_Player.position;
+    }
+
+
+}
